ccf33c32ba634d2089200b1d76ded2afbeedf5b1
Joaco <canteroxx92@gmail.com>
2023-06-28 11:43:03 -0400

    formulas

--- latex.py
+++ latex.py
@@ -3,7 +3,7 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg
 from PIL import Image, ImageTk
 
 def convert_latex_to_image(latex_formula, output_path):
-    fig = plt.figure(figsize=(2.4, 2))
+    fig = plt.figure(figsize=(2.4, 3))
     
     # Cambiar el color de fondo de la figura (ejemplo: #2f3123)
     fig.patch.set_facecolor('#2f3123')
@@ -26,7 +26,7 @@ def convert_latex_to_image(latex_formula, output_path):
 formula = r"$\theta$"
 formula2= r"$\mu$"
 formula3= r"$E_{ci}$"
-latex_formula = "Donde: \n  - W = Trabajo \n  - F = Fuerza \n  - D = Desplazamiento \n  - "+(formula)+" = Angulo \n  - "+(formula2)+" = Coficiente \n  de Roce "
+latex_formula = " Donde: \n  - W = Trabajo \n  - F = Fuerza \n  - D = Desplazamiento \n  - "+(formula)+" = Angulo \n  - "+(formula2)+" = Coficiente \n   de Roce \n  - m = Masa \n  - g = aceleracion \n   de gravedad"
 output_path = "latex_formula_8.png"
 convert_latex_to_image(latex_formula, output_path)
 print("La fórmula LaTeX se ha convertido en una imagen con fondo de color y letras blancas y se ha guardado en", output_path)
